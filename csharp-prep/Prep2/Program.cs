d29dda7325df17b102f412c3d93551eee088b505
mposadas1394 <104702037+mposadas1394@users.noreply.github.com>
2023-09-16 23:50:40 -0300

    prep2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,29 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("What´s your grade percentage");
+        string response=Console.ReadLine();
+        int grade=int.Parse(response);
+        if (grade>=90){
+            Console.WriteLine("You got an A")
+        }
+        else if(grade>=80){
+            Console.WriteLine("You got an B")
+        }
+        else if(grade>=70){
+            Console.WriteLine("You got an C")
+        }
+        else if(grade>=60){
+            Console.WriteLine("You got an D")
+        }
+        else {
+            Console.WriteLine("You got an F")
+        }
+        if(grade>=70){
+            Console.WriteLine("Congratulations! You passed the course")
+        }
+        else{
+            Console.WriteLine("Sorry, You didn´t pass. Try it again")
+        }
     }
 }
