a2a455abc37ea02ca3743901127e76b5c50664f4
mposadas1394 <104702037+mposadas1394@users.noreply.github.com>
2023-09-16 23:17:23 -0300

    first prep

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,10 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.WriteLine("What´s your first name?:");
+        string first_name=Console.ReadLine();
+        Console.WriteLine("What´s your last name?:");
+        string last_name=Console.ReadLine();
+        Console.Write($"Your name is {last_name}, {firs_name} {last_name}")
     }
 }
