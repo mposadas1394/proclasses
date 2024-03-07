af85ade81c9dc045dd102a8a2e52d458fdedf179
matias posadas <matiasposadas1394@gmail.com>
2024-03-06 22:00:44 -0300

    prep week 02

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1= new Job();
+        job1._company="Microsoft";
+        job1._jobTitle="Technician";
+        job1._startYear=2018;
+        job1._endYear=2020;
+        
+        Job job2= new Job();
+        job2._company="Apple";
+        job2._jobTitle="Software Engineer";
+        job2._startYear=2021;
+        job2._endYear=2022;
+        
+        Resume matias=new Resume();
+        matias._jobs.Add(job1);
+        matias._jobs.Add(job2);
+        matias._name="Matias Posadas";
+
+        matias.Display();
+        
     }
 }
