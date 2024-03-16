ef92a7c7a6ab097b5bc1674c5ace98257df848e7
matias posadas <matiasposadas1394@gmail.com>
2024-03-16 19:31:13 -0300

    scripture memorizer

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -4,16 +4,21 @@ using System.Linq.Expressions;
 using System.Runtime.CompilerServices;
 using System.Threading.Tasks.Dataflow;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 class Program
 {
     static void Main(string[] args)
     {
        
-         Reference passage=new Reference("juan",5, 12);
-         string text= "The quick brown fox jumps over the lazy dog.";
+         
+         
+         
+         Reference passage=new Reference("John",10, 11,12);
+         string text= "I am the good shepherd: the good shepherd giveth his life for the sheep.But he that is an hireling, and not the shepherd, whose own the sheep are not, seeth the wolf coming, and leaveth the sheep, and fleeth: and the wolf catcheth them, and scattereth the sheep.";
          Scripture content= new Scripture(passage,text);
          string answer=" ";
          bool isHidden=false;
+         
          do
          {content.GetDisplayScripture();
          content.HideRandomWords(3);
@@ -24,5 +29,16 @@ class Program
          isHidden=content.IsCompletelyHidden();
          }while((answer!="quit")&&(isHidden!=true) );
          
+         Console.WriteLine(" ");
+         Console.WriteLine(" ");
+
+         if(answer=="quit")
+         {
+            Console.WriteLine("Good luck next chance");
+         }
+         if(isHidden==true)
+         {
+            Console.WriteLine("All words are hidden");
+         }
     }
 }
